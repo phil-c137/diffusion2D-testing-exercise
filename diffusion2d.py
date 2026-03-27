640d74f34eec38ddd1881fab0b3487273decd2a3
Philipp Hofmann <philipp.hofmann@iws.uni-stuttgart.de>
2026-03-27 11:58:03 +0100

    Add assrtions inputs are floats

--- diffusion2d.py
+++ diffusion2d.py
@@ -38,6 +38,11 @@ class SolveDiffusion2D:
         self.dt = None
 
     def initialize_domain(self, w=10., h=10., dx=0.1, dy=0.1):
+        # Assert all inputs are floats
+        assert isinstance(w, float)
+        assert isinstance(h, float)
+        assert isinstance(dx, float)
+        assert isinstance(dy, float)
         self.w = w
         self.h = h
         self.dx = dx
@@ -46,6 +51,10 @@ class SolveDiffusion2D:
         self.ny = int(h / dy)
 
     def initialize_physical_parameters(self, d=4., T_cold=300, T_hot=700):
+        # Assert all inputs are floats
+        assert isinstance(d, float)
+        assert isinstance(T_cold, float)
+        assert isinstance(T_hot, float)
         self.D = d
         self.T_cold = T_cold
         self.T_hot = T_hot
